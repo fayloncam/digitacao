041aaf2b8ece37459f0572c0412d4a0cc1adc2a1
Faylon Camargo <92471708+fayloncam@users.noreply.github.com>
2021-10-24 20:26:24 -0300

    Atualização
    Progresso com a digitação e leitura das palavras

--- html-css/JavaScript/principal.js
+++ html-css/JavaScript/principal.js
@@ -2,18 +2,30 @@ var palDig = ''
 var cxTagDig = document.getElementById('digitacao')
 var cxPalLid = document.getElementById('palLer')
 var palLid = cxPalLid.innerText
+var proTagNum =''
 
-cxTagDig.addEventListener('keydown', function (e) {
-        palDig += e.key
+
+cxTagDig.addEventListener('keyup', function (e) {
+        //palDig += e.key
         var codTecla = e.code
         //console.log(`${codTecla}`)
         if (codTecla == 'Space') {
+            palDig = cxTagDig.value
             compPalavra()
-            //console.log(`${palDig}`)
+            //console.log(`${cxTagDig.innerText}`)
             //console.log(`${palLid}`)
         }
     })
 
 function compPalavra(){
+    
+    console.log(`${palDig}`)
     palDig == `${cxPalLid.innerText} ` ? cxPalLid.style.color = 'green' : cxPalLid.style.color = 'red'
+    palDig = ''
+    cxTagDig.value = ''
+    proTagNum ++
+    var proTag = `palLer${proTagNum}`
+    cxPalLid = document.getElementById(proTag)
+    //console.log(`${proTag}`)
+    //console.log(`${proTagNum}`)
 }
