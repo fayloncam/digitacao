4649c5a153dc2f2989fa1a860f7c42a29d4f4070
Faylon Camargo <92471708+fayloncam@users.noreply.github.com>
2021-10-26 21:06:17 -0300

    teste com keypress

--- html-css/JavaScript/principal.js
+++ html-css/JavaScript/principal.js
@@ -3,17 +3,34 @@ var cxTagDig = document.getElementById('digitacao')
 var cxPalLid = document.getElementById('palLer')
 var palLid = cxPalLid.innerText
 var proTagNum =''
+var bsCounter = Number('0')
+
+/*Variavel que seta o estilo caso seja digitado alguma letra seja pressionada errada
+
+cxTagDig.addEventListener('keypress', function(f) {
+    var codTeclaPress = f.key
+    if (codTeclaPress != palLid.charAt(0)){
+        console.log('diferente')
+    }
+    //console.log(`foi digitado ${codTeclaPress}`)
+})
+
+*/
 
 
 cxTagDig.addEventListener('keyup', function (e) {
         //palDig += e.key
         var codTecla = e.code
-        //console.log(`${codTecla}`)
+        
+        console.log(`${codTecla}`)
         if (codTecla == 'Space') {
             palDig = cxTagDig.value
             compPalavra()
             //console.log(`${cxTagDig.innerText}`)
             //console.log(`${palLid}`)
+        } else if (codTecla == 'Backspace') {
+            bsCounter ++
+            console.log(`${bsCounter}`)
         }
     })
 
