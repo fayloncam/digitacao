d55c6fb51b06145253f02f48574af1f30393d5c3
Faylon Camargo <faylon@gmail.com>
2022-06-14 00:00:44 -0300

    Trabalhando na caixa de leitura
    Colocando a caixa de leitura para colocar as palavras quando carregar

--- JavaScript/principal.js
+++ JavaScript/principal.js
@@ -1,9 +1,27 @@
+
+    
+    const portugues = ['coisa','casa','tempo','dia','ano','vez','homem','senhor','moça', 'bom', 'grande','melhor','pior','certo','último','próprio','ser','ir','estar','ter','haver','fazer','dar','ficar','poder','ver','não','mais','muito','já','quando','mesmo','depois','ainda','um','dois','primeiro','cem','mil','a','o','um','uma','de','em','para','por','com','até','e','mas','ou','também','se','assim','como','porque','que','eu','você','ele','este','esse','isso','sua','teste','digitar','João','Pedro','Marcos','Lucas','Mateus']
+    let tamanho = portugues.length
+    let randomiza = (tam) => Math.floor (Math.random() * tam)
+
+    var selecionaLeitura = document.querySelector('.leitura') // Cria variavel que recebe o elemento pai
+    var numTagInicio = 0
+    while (numTagInicio <= 200) {
+    let colocaSpan = document.createElement('span')// Cria variavel que recebe o tipo de tag a ser criada, na verdade cria o elemento
+    colocaSpan.innerText = `${portugues[randomiza(tamanho)]}` // Configura o texto (aqui a palavra) da tag que será criada
+    colocaSpan.id = `palLer${numTagInicio}` // Configura o id da tag que será criada
+    selecionaLeitura.insertAdjacentElement('beforeend',colocaSpan) // Cria de fato a tag levando em conta o pai (definido na variável) e adiciona-a
+    selecionaLeitura.insertAdjacentText('beforeend',' ')
+    numTagInicio++    
+    }
+    
+
+var cxPalLid = document.getElementById('palLer0')  
 var palDig = ''
-var cxTagDig = document.getElementById('digitacao')
-var cxPalLid = document.getElementById('palLer')
-var palLid = cxPalLid.innerText
 var proTagNum =''
 var bsCounter = Number('0')
+var cxTagDig = document.getElementById('digitacao')
+
 
 /*Variavel que seta o estilo caso seja digitado alguma letra seja pressionada errada
 
@@ -37,7 +55,7 @@ addEventListener('keyup', function (e) {
 
 function compPalavra(){
     
-    console.log(`${palDig}`)
+    //console.log(`${palDig}`)
     palDig == `${cxPalLid.innerText} ` ? cxPalLid.style.color = 'green' : cxPalLid.style.color = 'red'
     palDig = ''
     cxTagDig.value = ''
@@ -48,23 +66,8 @@ function compPalavra(){
     //console.log(`${proTagNum}`)
 }
 
-function troca_palavra () {
+/*function troca_palavra () {
     var coloca_palavra = document.getElementById('palLer')
     coloca_palavra.innerText = "trocado"
-}
- var numTagInicio = 4
-function colocaPalavras (){
-    
-    var portugues = ['coisa','casa','tempo','dia','ano','vez','homem','senhor','moça', 'bom', 'grande','melhor','pior','certo','último','próprio','ser','ir','estar','ter','haver','fazer','dar','ficar','poder','ver','não','mais','muito','já','quando','mesmo','depois','ainda','um','dois','primeiro','cem','mil','a','o','um','uma','de','em','para','por','com','até','e','mas','ou','também','se','assim','como','porque','que','eu','você','ele','este','esse','isso','sua']
-    var tamanho = portugues.length
-    tamanho++
-    var randomiza = (tam) => Math.floor (Math.random() * tam)
-    var selecionaLeitura = document.querySelector('.leitura') // Cria variavel que recebe o elemento pai
-    
-    const colocaSpan = document.createElement('span')// Cria variavel que recebe o tipo de tag a ser criada, na verdade cria o elemento
-    colocaSpan.innerText = `${portugues[randomiza(tamanho)]}` // Configura o texto (aqui a palavra) da tag que será criada
-    colocaSpan.id = `palLer${numTagInicio}` // Configura o id da tag que será criada
-    selecionaLeitura.insertAdjacentElement('beforeend',colocaSpan) // Cria de fato a tag levando em conta o pai (definido na variável) e adiciona-a
-    selecionaLeitura.insertAdjacentText('beforeend',' ')
-    numTagInicio++
-}
+}*/
+ 
